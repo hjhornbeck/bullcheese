85e1dc1dc7e02b198e8d85d4f5828992ce23c0da
hjhornbeck <hjhornbeck@protonmail.com>
2021-03-16 00:33:53 -0600

    - First commit! The encryption/decryption routines have been tested, and   seem to work A-OK.

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from random import choice
+
+web_site = Flask(__name__)
+
+number_list = [
+	100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415,
+	416, 417, 418, 421, 422, 423, 424, 425, 426,
+	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
+]
+
+@web_site.route('/')
+def index():
+	return render_template('index.html')
+
+@web_site.route('/user/', defaults={'username': None})
+@web_site.route('/user/<username>')
+def generate_user(username):
+	if not username:
+		username = request.args.get('username')
+
+	if not username:
+		return 'Sorry error something, malformed request.'
+
+	return render_template('personal_user.html', user=username)
+
+@web_site.route('/page')
+def random_page():
+  return render_template('page.html', code=choice(number_list))
+
+web_site.run(host='0.0.0.0', port=8080)
---
+++ utils/shared.py
@@ -0,0 +1,199 @@
+
+from cryptography.hazmat.backends import default_backend as backend
+from cryptography.hazmat.primitives import hashes, hmac, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+from secrets import randbits, token_bytes
+
+def hash_bytes( input, key=None ):
+    """Apply SHA2-256. Optionally use HMAC.
+
+    PARAMETERS
+    ==========
+    input: A bytes object to be encrypted and tagged.
+    key: A bytes object containing the key, or None if no key is being
+       used.
+
+    RETURN
+    ======
+    A bytes object.
+    """
+    assert type(input) is bytes
+    assert (key is None) or type(key) is bytes
+    assert (key is None) or (len(key) == 32)
+
+    if key is None:
+        tagger = hashes.Hash( hashes.SHA256(), backend=backend() )
+    else:
+        tagger = hmac.HMAC( key, hashes.SHA256(), backend=backend() )
+
+    tagger.update( input )
+    tag = tagger.finalize()
+    del tagger
+
+    return tag
+
+def encrypt_bytes( input, key ):
+    """Encrypt a byte sequence with AES. Length of key determines 
+       the cypher chosen.
+
+    PARAMETERS
+    ==========
+    input: A bytes object to be encrypted and tagged.
+    key: A bytes object containing the key.
+
+    RETURN
+    ======
+    A bytes object.
+    """
+
+    assert type(input) is bytes
+    assert type(key) is bytes
+    assert (len(key) == 16) or (len(key) == 24) or (len(key) == 32)
+
+    iv = token_bytes( len(key) )
+    tag = hash_bytes( input )
+
+    padder = padding.PKCS7( len(key)*8 ).padder()
+    padded = padder.update(input) + padder.update(tag) + \
+            padder.finalize()
+
+    cypher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend()).encryptor()
+    encrypt = iv + cypher.update(padded) + cypher.finalize()
+
+    del iv, tag, padder, padded, cypher # encourage GC
+    return encrypt
+
+def decrypt_bytes( input, key ):
+    """Decrypt a byte sequence with AES, if possible.
+
+    PARAMETERS
+    ==========
+    input: A bytes object to be decrypted and verified.
+    key: A bytes object containing the key.
+
+    RETURN
+    ======
+    If the input could be decrypted, return a bytes object.
+      Otherwise, return None.
+    """
+
+    assert type(input) is bytes
+    assert type(key) is bytes
+    assert (len(key) == 16) or (len(key) == 24) or (len(key) == 32)
+
+    if (len(input) % len(key)) != 0:    # only certain sizes are valid
+        return None
+
+    iv = input[:len(key)]
+
+    cypher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend()).decryptor()
+    padded = cypher.update(input[len(key):]) + cypher.finalize()
+    del iv, cypher      # encourage GC
+
+    padder = padding.PKCS7( len(key)*8 ).unpadder()
+    try:
+        tagged = padder.update(padded) + padder.finalize()
+    except:
+        del padder
+        return None
+    
+    tag = hash_bytes( tagged[:-32] )
+    if tag != tagged[-32:]:
+        del tagged, tag
+        return None
+
+    else:
+        del tag
+        return tagged[:-32]
+
+def generate_token( seed, cat, time, salt, key ):
+    """Generate the token used to validate a run.
+
+    PARAMETERS
+    ==========
+    seed: A bytes object representing the seed.
+    cat: An int representing the category the seed was drawn from.
+    time: The time the seed was drawn, in 1/16ths of a 
+       second past epoch.
+    salt: A bytes object containing this server's salt.
+    key: A bytes object containing the encryption key.
+
+    RETURN
+    ======
+    A bytes object.
+    """
+
+    assert type(seed) is bytes
+    assert len(seed) == 8
+    assert type(cat) is int
+    assert (cat >= 0) and (cat <= 255)
+    assert type(time) is int
+    assert (time >= 0) and (time <= 0xffffffff)
+    assert type(salt) is bytes
+    assert len(salt) == 32
+    assert type(key) is bytes
+    assert (len(key) == 16) or (len(key) == 24) or (len(key) == 32)
+
+    # create the token's core
+    core = seed + cat.to_bytes( 1, 'big' ) + time.to_bytes( 4, 'big' )
+
+    # create the associated tag
+    tag = hash_bytes( core, salt )
+
+    # combine them into the appropriate length
+    raw_token = core + tag[: len(key) - len(core)]
+
+    # finally, encrypt and return
+    cypher = Cipher(algorithms.AES(key), mode=modes.ECB(), backend=backend()).encryptor()
+    token = cypher.update( raw_token ) + cypher.finalize()
+
+    del core, tag, raw_token, cypher
+    return token
+
+def decrypt_token( seed, token, salt, key ):
+    """Decrypt and validate the token. "Validate" means check
+       that the provided seed matches the one inside the token, and
+       the pseudo-nonce is as expected.
+
+    PARAMETERS
+    ==========
+    seed: A bytes object representing the seed.
+    token: A bytes object of the token to validate.
+    salt: A bytes object containing this server's salt.
+    key: A bytes object containing the encryption key.
+
+    RETURN
+    ======
+    If the token is invalid, returns None. If it is valid, returns a 
+      tuple consisting of (seed, cat, time), where
+       seed is a bytes object representing the Minecraft seed,
+       cat is an int representing the category the seed was drawn from,
+       and time is the time the seed was drawn, in 1/16th of a second past epoch.
+    """
+    assert type(seed) is bytes
+    assert len(seed) == 8
+    assert type(token) is bytes
+    assert (len(token) == 16) or (len(token) == 24) or (len(token) == 32)
+    assert type(salt) is bytes
+    assert len(salt) == 32
+    assert type(key) is bytes
+    assert len(key) == len(token)
+
+    # decrypt the token
+    cypher = Cipher(algorithms.AES(key), mode=modes.ECB(), backend=backend()).decryptor()
+    raw_token = cypher.update( token ) + cypher.finalize()
+    del cypher
+
+    # check that the seeds match
+    if raw_token[:8] != seed:
+        return None
+
+    # check the pseudo-nonce (the "core" is 13 bytes long)
+    tag = hash_bytes( raw_token[:13], salt )
+    if tag[: len(key) - 13] != raw_token[13:]:
+        del tag
+        return None
+    else:
+        return seed, raw_token[8], \
+                int.from_bytes( raw_token[9:13], 'big' )
