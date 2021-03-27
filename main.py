f91df739175f4c7b42eddf3cce786a284d81d2b2
hjhornbeck <hjhornbeck@protonmail.com>
2021-03-27 12:36:26 -0600

    - STILL BROKEN. Decided to use a more object-oriented approach to the   web internals. Wrote a decent chunk of a generic Category class for seed categories. - Added some additional global variables, to give a bit more flexibility   about where some data is stored.

--- main.py
+++ main.py
@@ -22,6 +22,15 @@ BLOCKS = 2              # 2 is more annoying for end users, but vastly more secu
 # how many seconds do we wait for a lock until giving up?
 TIMEOUT = 15
 
+# what directory are the seeds stored in?
+SEED_DIR = "seeds"
+
+# how about the HTML templates?
+TEMPLATE_DIR = "templates"
+
+# and where is our scratch hard drive space?
+TMP_DIR = "/tmp"
+
 # beyond this point, you shouldn't have to edit any variables manually
 
 
@@ -29,12 +38,16 @@ TIMEOUT = 15
 
 from binascii import unhexlify
 
+import errno
+
 from flask import Flask, render_template, request
 from filelock import Timeout, FileLock
 
-from os import getenv           # for loading secrets on repl.it
+from math import ceil, log2
 
-from secrets import token_bytes # secure RNG
+from os import getenv, strerror
+
+from secrets import randbits, token_bytes
 
 from time import time
 from typing import Optional
@@ -47,7 +60,71 @@ from utils.fsg_ticket import generate_ticket, hash_bytes, pretty_ticket
 from utils.fsg_ticket import unsigned_to_signed
 
 
+##### CLASSES
+
+class Category:
+    """An abstract representation of a seed category. The brains of the operation."""
+    def __init__(self, num: int):
+        """Create a Category, given the following arguments.
+
+        PARAMETERS
+        ==========
+        num:             The category's number. Must be positive or zero.
+        """
+        global FORGE_SUCCESS, LD50, SALT, TIMEOUT, TMP_DIR
+
+        assert num > 0      # unsigned only!
+
+        self.numeric = num
+
+        # load the seed file early so we can signal quickly
+        self.url, self.name, self.seeds = load_seeds( self.seed_file() )
+        if self.seeds is None:
+            raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), self.seed_file())
+
+        self.seed_count = len(seeds) >> 3
+        self.seed_bits  = int(ceil(log2( self.seed_count )))
+
+        # calculate the generate and verify interval from the number of seeds
+        self.gen    = gen_interval
+        self.verify = verify_interval
+
+        # generate the filenames and locks 
+        gen_lock_file = hash_bytes( f"{num:03d}.generate.lock", SALT ).hex()
+        self.gen_lock = FileLock( f"{TMP_DIR}/{gen_lock_file}", timeout=TIMEOUT )
+        self.gen_file = TMP_DIR + '/' + hash_bytes( f"{num:03d}.generate.file", SALT ).hex()
+
+        ver_lock_file = hash_bytes( f"{num:03d}.verify.lock", SALT ).hex()
+        self.ver_lock = FileLock( f"{TMP_DIR}/{ver_lock_file}", timeout=TIMEOUT )
+        self.ver_file = TMP_DIR + '/' + hash_bytes( f"{num:03d}.verify.file", SALT ).hex()
+
+    def seed_file(self):
+        """Return the file associated with this seed category."""
+        global SEED_DIR
+
+        return f"{SEED_DIR}/{self.numeric:03d}.seeds.gz"
+
+    def generate(self):
+        """Generate a ticket. A smart wrapper around generate_ticket().
+
+        RETURN
+        ======
+        A tuple of the form (seed, time, ticket), where seed is an int,
+           time is a datetime object, and ticket a string.
+        """
+        global BLOCKS, PRIVATE_KEY, SALT
+
+        # acquire lock
+        # read last access time
+        # too quick? sleep
+        # write current time
+        # release lock
+        # randomly pick seed
+        # pass to generate_ticket()
+        # return
+
 ##### METHODS
+
 def get_key() -> tuple[bytes,bool]:
     """Retrieve the private key, encoded in hexadecimal. If not present, 
        pick a value randomly.
@@ -82,6 +159,14 @@ def get_salt() -> tuple[bytes,bool]:
         pass
 
     return token_bytes(64), True
+
+##### GENERATED VARIABLES
+
+PRIVATE_KEY, RANDOM_KEY = get_key()
+SALT, RANDOM_SALT       = get_salt()
+
+url_map = dict()        # for mapping between url names and category numbers
+cat_map = dict()        # for mapping between category numbers and classes
 
 ##### MAIN
 
